52c4c34e69c3951cd07bcb43d791fcd7ad639933
AlexBeattie42 <30098201+alexbeattie42@users.noreply.github.com>
2024-12-02 09:27:26 +0200

    Update windowing to use 100 after minimum

--- 03-window-training-data.py
+++ 03-window-training-data.py
@@ -36,7 +36,8 @@ def process_file_group(parent_suffix, file_group):
             index_z = np.where(np.abs(output_df[z_force_key])>5)[0]
             if index_z.size > 0:
                 min_index = index_z.min()
-                max_index = index_z.max()
+                max_index = min_index + 100
+                print(f"min: {min_index} and max: {max_index}")
                 #extended window length
 
                 windowed_out = output_df.iloc[min_index:max_index + 1]
